3f248199ff256d1a098f44f4efe5168ef146fb85
Kevin <kevin@pirih.de>
2017-12-01 23:23:11 +0100

    Inital Commit

---
+++ etc/openhab2/scripts/oh_itunes-api.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+################################################################################
+#BashScript zur Steuerung der iTunes-Api in Openhab - Version 0.3 by eiGelbGeek#
+#https://github.com/maddox/itunes-api by maddox                                #
+################################################################################
+
+itunesAPI_URL="XXX.XXX.XXX.XXX"
+itunesAPI_Port="8181"
+
+openhab_url="XXX.XXX.XXX.XXX"
+openhab_port="8080"
+
+#Ab hier sind keine Änderungen mehr nötig!
+case $1 in
+    volume)
+        #$2 = 0-100 $3 = Airplay_ID
+        curl -X PUT -d "level=$2" http://$itunesAPI_URL:$itunesAPI_Port/airplay_devices/$3/volume
+        ;;
+    get_set_volume)
+        #$2 = Airplay_ID - $3 = Openhab Dimmer für Volume
+        if AP_Volume="$(curl -s GET --header "Accept: application/json" "http://$itunesAPI_URL:$itunesAPI_Port/airplay_devices/$2" | jq -r '.sound_volume' 2>/dev/null)";
+        then
+          curl -X POST --header "Content-Type: text/plain" --header "Accept: application/json" -d "$AP_Volume" "http://$openhab_url:$openhab_port/rest/items/$3"
+        else
+          curl -X POST --header "Content-Type: text/plain" --header "Accept: application/json" -d "0" "http://$openhab_url:$openhab_port/rest/items/$3"
+          exit 1
+        fi
+        ;;
+    play)
+        curl -X PUT http://$itunesAPI_URL:$itunesAPI_Port/play
+        ;;
+    playpause)
+        curl -X PUT http://$itunesAPI_URL:$itunesAPI_Port/playpause
+        ;;
+    pause)
+        curl -X PUT http://$itunesAPI_URL:$itunesAPI_Port/pause
+        ;;
+    stop)
+        curl -X PUT http://$itunesAPI_URL:$itunesAPI_Port/stop
+        ;;
+    previous)
+        curl -X PUT http://$itunesAPI_URL:$itunesAPI_Port/previous
+        ;;
+    next)
+        curl -X PUT http://$itunesAPI_URL:$itunesAPI_Port/next
+        ;;
+    mute)
+        #$2 = true/false
+        curl -X PUT -d "muted=$2" http://$itunesAPI_URL:$itunesAPI_Port/mute
+        ;;
+    shuffle)
+        #$2 = off/songs/albums/groupings
+        curl -X PUT -d "mode=$2" http://$itunesAPI_URL:$itunesAPI_Port/shuffle
+        ;;
+    repeat)
+        #$2 = off/one/all
+        curl -X PUT -d "mode=$2" http://$itunesAPI_URL:$itunesAPI_Port/repeat
+        ;;
+    airplay_on_off)
+        #$2 = Airplay_ID - $3 = on/off - $4 = Openhab Switch Item für Airplay Device Status
+        if AP_Selected="$(curl -s GET --header "Accept: application/json" "http://$itunesAPI_URL:$itunesAPI_Port/airplay_devices/$2" | jq -r '.selected' 2>/dev/null)";
+        then
+          if $AP_Selected == "true";
+          then
+            curl -X POST --header "Content-Type: text/plain" --header "Accept: application/json" -d "ON" "http://$openhab_url:$openhab_port/rest/items/$4"
+          else
+            curl -X PUT http://$itunesAPI_URL:$itunesAPI_Port/airplay_devices/$2/$3
+            curl -X POST --header "Content-Type: text/plain" --header "Accept: application/json" -d "ON" "http://$openhab_url:$openhab_port/rest/items/$4"
+          fi
+        else
+          curl -X POST --header "Content-Type: text/plain" --header "Accept: application/json" -d "OFF" "http://$openhab_url:$openhab_port/rest/items/$4"
+        	exit 1
+        fi
+        ;;
+    playlist)
+        #$2 = Playlist ID
+        playlist_ids=( one two three )
+        curl -X PUT http://$itunesAPI_URL:$itunesAPI_Port/playlists/${playlist_ids[$2]}/play
+        ;;
+     *)
+        echo "Kommando nicht vorhanden!"
+        ;;
+esac
---
+++ etc/openhab2/scripts/oh_refresh_playlist_itunes-api.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+#######################################################################################
+#BashScript zur Dynamischen Playlist Verwaltung in Openhab - Version 0.3 by eiGelbGeek#
+#######################################################################################
+
+itunesAPI_URL="XXX.XXX.XXX.XXX"
+itunesAPI_Port="8181"
+oh_sitemap_dir="/etc/openhab2/sitemaps"
+oh_sitemap_name="YOURSITEMAP.sitemap"
+oh_script_dir="/etc/openhab2/scripts"
+oh_playlist_item="playlist_selection"
+
+###########################################
+#Ab hier sind keine Änderungen mehr nötig!#
+###########################################
+
+> /tmp/itunes_playlist_names.txt
+> /tmp/itunes_playlist_ids.txt
+#Playlisten Namen abfragen und in Temporäre Datei schreiben!
+curl -s GET --header "Accept: application/json" "http://$itunesAPI_URL:$itunesAPI_Port/playlists" | jq '.playlists[] | .name' > /tmp/itunes_playlist_names.txt
+#Zeile 1-9 löschen! (iTunes Standard Playlisten!!)
+sed -i 1,9D /tmp/itunes_playlist_names.txt
+#Erforderliche Formatierung für Sitemap hinzufügen!
+cp /tmp/itunes_playlist_names.txt /tmp/itunes_playlist_names_tmp.txt
+awk '{print NR"="$0", "}' /tmp/itunes_playlist_names_tmp.txt > /tmp/itunes_playlist_names.txt
+rm /tmp/itunes_playlist_names_tmp.txt
+#Datei vervollständigen!
+sed -i '1 i\0="Playlist auswählen",' /tmp/itunes_playlist_names.txt
+sed -i '1 i\Selection item=playlist_selection label="Playliste" icon="playlist" mappings=[' /tmp/itunes_playlist_names.txt
+sed -i '$s/.\{2\}$//' /tmp/itunes_playlist_names.txt
+echo "]" >> /tmp/itunes_playlist_names.txt
+cp /tmp/itunes_playlist_names.txt /tmp/itunes_playlist_names_tmp.txt
+tr -s '\n' ' ' < /tmp/itunes_playlist_names_tmp.txt > /tmp/itunes_playlist_names.txt
+rm /tmp/itunes_playlist_names_tmp.txt
+varPlaylistNames=$(cat /tmp/itunes_playlist_names.txt)
+#Playlist Selection in Sitemap suchen und ersetzen
+sed -i 's/\Selection item='"$oh_playlist_item"'\b.*/'"$varPlaylistNames"'/g' $oh_sitemap_dir/$oh_sitemap_name
+#Dateirechte setzen!
+cd $oh_sitemap_dir
+chown openhab:openhab $oh_sitemap_name
+
+#Playlisten IDs abfragen und in Temporäre Datei schreiben!
+curl -s GET --header "Accept: application/json" "http://$itunesAPI_URL:$itunesAPI_Port/playlists" | jq -r '.playlists[] | .id' > /tmp/itunes_playlist_ids.txt
+#Zeile 1-9 löschen! (iTunes Standard Playlisten!!)
+sed -i 1,9D /tmp/itunes_playlist_ids.txt
+#Datei vervollständigen!
+sed -i '1 i\playlist_ids=( dummy' /tmp/itunes_playlist_ids.txt
+echo ")" >> /tmp/itunes_playlist_ids.txt
+cp /tmp/itunes_playlist_ids.txt /tmp/itunes_playlist_ids_tmp.txt
+tr -s '\n' ' ' < /tmp/itunes_playlist_ids_tmp.txt > /tmp/itunes_playlist_ids.txt
+rm /tmp/itunes_playlist_ids_tmp.txt
+varPlaylistIDs=$(cat /tmp/itunes_playlist_ids.txt)
+sed -i 's/\'"playlist_ids=( "'\b.*/'"$varPlaylistIDs"'/g' $oh_script_dir/oh_itunes-api.sh
+#Dateirechte setzen!
+cd $oh_script_dir
+chown openhab:openhab oh_itunes-api.sh
+chmod u+x oh_itunes-api.sh
